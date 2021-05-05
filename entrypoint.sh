9bb87fbe24a4cca3a0ded39b1abf0fe50e979db2
wuzonghan <han.wu@ichef.com.tw>
2021-05-05 17:04:49 +0800

    feat:update action.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-echo  -e "test123 \n"
-echo `date`
+echo "run script"
+
+curl -s https://codecov.io/bash > codecov;
+VERSION=$(grep 'VERSION=\"[0-9\.]*\"' codecov | cut -d'"' -f2);
+echo $VERSION
+for i in 1 256 512
+do
+shasum -a $i -c <(curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM" | head -n 1)
+done
+rm codecov
+
+echo "end"
