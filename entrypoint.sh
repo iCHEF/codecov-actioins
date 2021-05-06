69ecc78a82659ba2d30e36b7fd5da55d3bb4e8e2
wuzonghan <han.wu@ichef.com.tw>
2021-05-06 12:06:55 +0800

    feat:update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "run script"
-
 curl -s https://codecov.io/bash > codecov;
 VERSION=$(grep 'VERSION=\"[0-9\.]*\"' codecov | cut -d'"' -f2);
 VERSION=1.11.0
@@ -12,14 +10,12 @@ do
 shasum -a $i -c <(curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM" | head -n 1)
   if [[ $? -eq 0 ]]
   then
-    echo "success"
+    echo "checksum validation success"
     bash <(curl -s https://codecov.io/bash) -f "./coverage/cobertura-coverage.xml"
     exit 0
   else
-    echo "checksum fail"
+    echo "checksum validation fail"
     exit 1
   fi
 done
 rm codecov
-
-echo "end"
